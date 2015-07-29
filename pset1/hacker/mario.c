821f8eb4fad9d54a124fe933f7e96467825d9a56
Christian Javellana <cjjavellana@gmail.com>
2015-07-29 19:23:33 -0400

    Moved magic numbers to constants

--- pset1/hacker/mario.c
+++ pset1/hacker/mario.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <cs50.h>
 
+#define HEIGHT_LOWER_LIMIT 0
+#define HEIGHT_UPPER_LIMIT 23
+#define GAP_WIDTH          2
+
 int main(void)
 {
     int height;
@@ -9,14 +13,14 @@ int main(void)
     {
         printf("Height: ");
         height = GetInt();
-    } while(height < 0 || height > 23);
+    } while(height < HEIGHT_LOWER_LIMIT || height > HEIGHT_UPPER_LIMIT);
 
     for(int i = 0; i < height; i++)
     {
-        for(int j = 0; j < ((height * 2) + 2); j++)
+        for(int j = 0; j < ((height * 2) + GAP_WIDTH); j++)
         {
             int printStart = height - i - 1;
-            int printEnd = height + i + 2;
+            int printEnd = height + i + GAP_WIDTH;
 
             if (j >= printStart && j <= printEnd && j != height && j != (height + 1))
             {
