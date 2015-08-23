8fb48954818e1bcfee3787a676dab7be8104e1ad
Christian Javellana <cjjavellana@gmail.com>
2015-08-23 05:23:03 -0400

    Removed binaries, implemented sort (selection sort), and binary search (recursive)

--- pset3/find/helpers.c
+++ pset3/find/helpers.c
@@ -11,14 +11,37 @@
 
 #include "helpers.h"
 
+static bool binary_search(int value, int values[], int lower_bound, int upper_bound);
+    
 /**
  * Returns true if value is in array of n values, else false.
  */
 bool search(int value, int values[], int n)
 {
     // TODO: implement a searching algorithm
+    return binary_search(value, values, 0, n);
+}
+
+static bool binary_search(int value, int values[], int lower_bound, int upper_bound)
+{
+    int mid = ((upper_bound - lower_bound) / 2) + lower_bound;
+    if (value == values[mid])
+    {
+        return true;
+    }
+        
+    if (value < values[mid])
+    {
+        return binary_search(value, values, lower_bound, mid);
+    }
+    else if(value > values[mid])
+    {
+        return binary_search(value, values, mid, upper_bound);
+    }
+
     return false;
 }
+
 
 /**
  * Sorts array of n values.
@@ -26,5 +49,24 @@ bool search(int value, int values[], int n)
 void sort(int values[], int n)
 {
     // TODO: implement an O(n^2) sorting algorithm
+
+    for (int i = 0, k = 0; i < n; i++)
+    {
+        k = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (values[j] < values[k]) {
+                k = j;
+            }
+        }
+
+        if (k != i) 
+        {
+            int temp = values[k];
+            values[k] = values[i];
+            values[i] = temp;
+        }
+    }
+
     return;
 }
