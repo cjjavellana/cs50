78fce0c1ccb602f54f54095653ad8e810f01750b
Christian Javellana <cjjavellana@gmail.com>
2015-10-01 21:25:57 -0400

    Initial commit: Stack sample

---
+++ samples/stack/stack.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define STACK_CAPACITY 65535
+
+typedef struct {
+    void *element[STACK_CAPACITY];
+    int count;
+} STACK;
+
+/**
+ * Removes an element from the stack
+ */
+void *pop(STACK *stack);
+
+/**
+ * Adds an element into the stack
+ */
+void push(STACK *stack, void *element);
+
+int main(int argc, char *argv[]){
+    STACK stack;
+    stack.count = 0;
+
+    push(&stack, (void *) 1);
+    push(&stack, (void *) 2);
+    push(&stack, (void *) 3);
+    push(&stack, (void *) 4);
+
+    printf("Popped Value: %d\n", (int) pop(&stack));
+    printf("Popped Value: %d\n", (int) pop(&stack));
+    printf("Popped Value: %d\n", (int) pop(&stack));
+    printf("Popped Value: %d\n", (int) pop(&stack));
+
+    return 0;
+}
+
+void push(STACK *stack, void *element) {
+    if (stack == NULL) {
+        printf("Error: Stack is null\n");
+        exit(1);
+    }
+
+    stack->element[stack->count] = element;
+    stack->count++;
+}
+
+void *pop(STACK *stack) {
+    stack->count--;
+    return stack->element[stack->count];
+}
